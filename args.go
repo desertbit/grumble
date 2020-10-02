d405583985d4daac8dd588fa0a100af86b39908f
Sebastian <sebastian@desertbit.com>
2020-10-02 16:06:17 +0200

    cleanup args, removing a lot of boilerplate

--- args.go
+++ args.go
@@ -26,13 +26,14 @@ package grumble
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"time"
 )
 
-type parseArgFunc func(args []string, res ArgMap) ([]string, bool, error)
-type defaultArgFunc func(res ArgMap)
+// The parseArgFunc describes a func that parses from the given command line arguments
+// the values for its argument and saves them to the ArgMap.
+// It returns the not-consumed arguments and an error.
+type parseArgFunc func(args []string, res ArgMap) ([]string, error)
 
 type argItem struct {
 	Name            string
@@ -41,29 +42,20 @@ type argItem struct {
 	HelpShowDefault bool
 	Default         interface{}
 
+	parser   parseArgFunc
 	isList   bool
 	optional bool
 }
 
 // Args holds all the registered args.
 type Args struct {
-	parsers  []parseArgFunc
-	defaults []defaultArgFunc
-	list     []*argItem
-}
-
-// sort the args by their name.
-func (a *Args) sort() {
-	sort.Slice(a.list, func(i, j int) bool {
-		return a.list[i].Name < a.list[j].Name
-	})
+	list []*argItem
 }
 
 func (a *Args) register(
 	name, help, helpArgs string,
 	helpShowDefault, isList, optional bool,
 	defaultValue interface{},
-	df defaultArgFunc,
 	pf parseArgFunc,
 ) {
 	// Validate.
@@ -99,11 +91,10 @@ func (a *Args) register(
 		HelpShowDefault: helpShowDefault,
 		HelpArgs:        helpArgs,
 		Default:         defaultValue,
+		parser:          pf,
 		isList:          isList,
 		optional:        optional,
 	})
-	a.defaults = append(a.defaults, df)
-	a.parsers = append(a.parsers, pf)
 }
 
 // empty returns true, if the args are empty.
@@ -115,21 +106,23 @@ func (a *Args) parse(args []string, res ArgMap) ([]string, error) {
 	// Iterate over all arguments that have been registered.
 	// There must be either a default value or a value available,
 	// otherwise the argument is missing.
-	var (
-		err    error
-		parsed bool
-	)
-	for i, p := range a.parsers {
-		args, parsed, err = p(args, res)
-		if err != nil {
-			return nil, err
-		} else if !parsed {
-			// Assign default value to it.
-			if a.defaults[i] == nil {
-				return nil, fmt.Errorf("missing argument '%s'", a.list[i].Name)
+	var err error
+	for _, item := range a.list {
+		// If no arguments are left, simply set the default values.
+		if len(args) == 0 {
+			// Check, if the argument is mandatory.
+			if !item.optional {
+				return nil, fmt.Errorf("missing argument '%s'", item.Name)
 			}
 
-			a.defaults[i](res)
+			// Register its default value.
+			res[item.Name] = &ArgMapItem{Value: item.Default, IsDefault: true}
+			continue
+		}
+
+		args, err = item.parser(args, res)
+		if err != nil {
+			return nil, err
 		}
 	}
 
@@ -138,77 +131,44 @@ func (a *Args) parse(args []string, res ArgMap) ([]string, error) {
 
 // String registers a string argument.
 func (a *Args) String(name, help, defaultValue string, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "string", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "string", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			res[name] = &ArgMapItem{Value: args[0]}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // StringList registers a string list argument.
 func (a *Args) StringList(name, help string, defaultValue []string, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "string list", true, true, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "string list", true, true, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			res[name] = &ArgMapItem{Value: args}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Bool registers a bool argument.
 func (a *Args) Bool(name, help string, defaultValue, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "bool", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "bool", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			b, err := strconv.ParseBool(args[0])
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid bool value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid bool value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: b}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // BoolList registers a bool list argument.
 func (a *Args) BoolList(name, help string, defaultValue []bool, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "bool list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "bool list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			var (
 				err error
@@ -217,51 +177,36 @@ func (a *Args) BoolList(name, help string, defaultValue []bool, optional bool) {
 			for i, a := range args {
 				bs[i], err = strconv.ParseBool(a)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid bool value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid bool value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: bs}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Int registers an int argument.
 func (a *Args) Int(name, help string, defaultValue int, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "int", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "int", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := strconv.Atoi(args[0])
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid int value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid int value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // IntList registers an int list argument.
 func (a *Args) IntList(name, help string, defaultValue []int, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "int list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "int list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				is  = make([]int, len(args))
@@ -269,51 +214,36 @@ func (a *Args) IntList(name, help string, defaultValue []int, optional bool) {
 			for i, a := range args {
 				is[i], err = strconv.Atoi(a)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid int value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid int value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Int64 registers an int64 argument.
 func (a *Args) Int64(name, help string, defaultValue int64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "int64", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "int64", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := strconv.ParseInt(args[0], 10, 64)
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid int64 value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid int64 value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // Int64List registers an int64 list argument.
 func (a *Args) Int64List(name, help string, defaultValue []int64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "int64 list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "int64 list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				is  = make([]int64, len(args))
@@ -321,51 +251,36 @@ func (a *Args) Int64List(name, help string, defaultValue []int64, optional bool)
 			for i, a := range args {
 				is[i], err = strconv.ParseInt(a, 10, 64)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid int64 value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid int64 value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Uint registers an uint argument.
 func (a *Args) Uint(name, help string, defaultValue uint, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "uint", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "uint", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := strconv.ParseUint(args[0], 10, 64)
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid uint value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid uint value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // UintList registers an uint list argument.
 func (a *Args) UintList(name, help string, defaultValue []uint, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "uint list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "uint list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				u   uint64
@@ -374,52 +289,37 @@ func (a *Args) UintList(name, help string, defaultValue []uint, optional bool) {
 			for i, a := range args {
 				u, err = strconv.ParseUint(a, 10, 64)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid uint value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid uint value '%s' for argument: %s", a, name)
 				}
 				is[i] = uint(u)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Uint64 registers an uint64 argument.
 func (a *Args) Uint64(name, help string, defaultValue uint64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "uint64", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "uint64", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := strconv.Atoi(args[0])
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid uint64 value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid uint64 value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // Uint64List registers an uint64 list argument.
 func (a *Args) Uint64List(name, help string, defaultValue []uint64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "uint64 list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "uint64 list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				is  = make([]uint64, len(args))
@@ -427,51 +327,36 @@ func (a *Args) Uint64List(name, help string, defaultValue []uint64, optional boo
 			for i, a := range args {
 				is[i], err = strconv.ParseUint(a, 10, 64)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid uint64 value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid uint64 value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Float64 registers a float64 argument.
 func (a *Args) Float64(name, help string, defaultValue float64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "float64", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "float64", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := strconv.ParseFloat(args[0], 64)
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid float64 value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid float64 value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // Float64List registers an float64 list argument.
 func (a *Args) Float64List(name, help string, defaultValue []float64, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "float64 list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "float64 list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				is  = make([]float64, len(args))
@@ -479,51 +364,36 @@ func (a *Args) Float64List(name, help string, defaultValue []float64, optional b
 			for i, a := range args {
 				is[i], err = strconv.ParseFloat(a, 64)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid float64 value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid float64 value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
 
 // Duration registers a duration argument.
 func (a *Args) Duration(name, help string, defaultValue time.Duration, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "duration", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
-			if len(args) == 0 {
-				return args, false, nil
-			}
+	a.register(name, help, "duration", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 
 			i, err := time.ParseDuration(args[0])
 			if err != nil {
-				return nil, false, fmt.Errorf("invalid duration value '%s' for argument: %s", args[0], name)
+				return nil, fmt.Errorf("invalid duration value '%s' for argument: %s", args[0], name)
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: i}
-			return args[1:], true, nil
+			return args[1:], nil
 		},
 	)
 }
 
 // DurationList registers an duration list argument.
 func (a *Args) DurationList(name, help string, defaultValue []time.Duration, optional bool) {
-	var df defaultArgFunc
-	if optional {
-		df = func(res ArgMap) { res[name] = &ArgMapItem{Value: defaultValue, IsDefault: true} }
-	}
-
-	a.register(name, help, "duration list", true, false, optional, defaultValue, df,
-		func(args []string, res ArgMap) ([]string, bool, error) {
+	a.register(name, help, "duration list", true, false, optional, defaultValue,
+		func(args []string, res ArgMap) ([]string, error) {
 			var (
 				err error
 				is  = make([]time.Duration, len(args))
@@ -531,13 +401,12 @@ func (a *Args) DurationList(name, help string, defaultValue []time.Duration, opt
 			for i, a := range args {
 				is[i], err = time.ParseDuration(a)
 				if err != nil {
-					return nil, false, fmt.Errorf("invalid duration value '%s' for argument: %s", a, name)
+					return nil, fmt.Errorf("invalid duration value '%s' for argument: %s", a, name)
 				}
 			}
 
-			// Set next argument as value.
 			res[name] = &ArgMapItem{Value: is}
-			return []string{}, true, nil
+			return []string{}, nil
 		},
 	)
 }
